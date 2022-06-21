2ca64936268194b5f375fbdad4a3c43b0443eeb0
danielhawkesford <daniel.hawkesford@gmail.com>
2022-06-21 10:05:55 +0100

    Built ResultsList and Result components

--- bootcamp-buddy/src/components/App/index.js
+++ bootcamp-buddy/src/components/App/index.js
@@ -1,9 +1,41 @@
 import './App.css';
+import { ResultsList } from '../ResultsList';
+
+const resources = [
+  {
+      URL: 'https://learngitbranching.js.org/',
+      title: 'Learn Git Branching',
+      type: 'Game/App',
+      topic: 'Git',
+      description: "LearnGitBranching is a git repository visualizer, sandbox, and a series of educational tutorials and challenges. Its primary purpose is to help developers understand git through the power of visualization (something that's absent when working on the command line). This is achieved through a game with different levels to get acquainted with the different git commands."
+  },
+  {
+      URL: 'https://testing-library.com/docs/react-testing-library/cheatsheet/',
+      title: 'A cheatsheet for the React Testing Library',
+      type: 'Documentation',
+      topic: 'React',
+      description: "A short guide to all the exported functions in React Testing Library"
+  },
+  {
+      URL: 'https://github.com/testing-library/jest-dom/blob/main/README.md',
+      title: 'A list of custom jest matchers to test the state of the DOM',
+      type: 'Documentation',
+      topic: 'Jest',
+      description: "The @testing-library/jest-dom library provides a set of custom jest matchers that you can use to extend jest. These will make your tests more declarative, clear to read and to maintain."
+  },
+  {
+      URL: 'https://linguinecode.com/post/react-usereducer-vs-usestate',
+      title: 'React useReducer vs React useState: When to use one over the other?',
+      type: 'Article',
+      topic: 'React',
+      description: "An article about what React.useReducer is used for, and when to use it instead ofReact.useState"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-    Hello 
+      <ResultsList results={resources} />
     </div>
   );
 }
---
+++ bootcamp-buddy/src/components/Result/index.js
@@ -0,0 +1,12 @@
+export function Result({ result }) {
+    return (
+        <div className="result">
+            <div>
+                <h3>{result.title}</h3>
+                <h3>{result.type}</h3>
+                <h3>{result.topic}</h3>
+            </div>
+            <p>{result.description}</p>
+        </div>
+    )
+}

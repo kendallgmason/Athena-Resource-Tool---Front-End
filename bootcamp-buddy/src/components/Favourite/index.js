794d91bea9966d7238e4927fa906dd10fa142048
danielhawkesford <daniel.hawkesford@gmail.com>
2022-06-21 17:35:19 +0100

    Updating CSS

--- bootcamp-buddy/src/components/Favourite/index.js
+++ bootcamp-buddy/src/components/Favourite/index.js
@@ -4,19 +4,18 @@ import "../App/App.css";
 export function Favourite({ favourites }) {
     return (
         <div className="favourites-content">
+            <h2>My Favourites</h2>
             <ul>
                 {favourites.map(favourite => 
-                    (<li Style={"list-style: none"}>
-                        <div className="favourite">
-                            <img src="star.svg" alt="A star"/>
-                            <a href={favourite.URL}>
-                                <h3>{favourite.title}</h3>
-                            </a>
-                        </div>
+                    (<li Style={"list-style: none"} className="favourite">
+                        <img src="star.svg" alt="A star"/>
+                        <a href={favourite.URL}>
+                            <h3>{favourite.title}</h3>
+                        </a>
                     </li>)
                 )}
             </ul>
-        <button id="save-button" type="button">Save</button>
+            <button id="save-button" type="button">Save</button>
         </div>
     );
 }

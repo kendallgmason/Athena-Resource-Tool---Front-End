7092c94190eb102889c1ce91b8d614c9df8375ae
danielhawkesford <daniel.hawkesford@gmail.com>
2022-06-21 10:53:32 +0100

    Adding solid line between results

--- bootcamp-buddy/src/components/Result/index.js
+++ bootcamp-buddy/src/components/Result/index.js
@@ -1,14 +1,17 @@
 export function Result({ result }) {
     return (
-        <div className="result">
             <div>
-                <h3>
-                    {result.title}
-                </h3>
-                <h3>{result.type}</h3>
-                <h3>{result.topic}</h3>
+                <div className="result">
+                    <div>
+                        <h3>
+                            {result.title}
+                        </h3>
+                        <h3>{result.type}</h3>
+                        <h3>{result.topic}</h3>
+                    </div>
+                    <p>{result.description}</p>
+                </div>
+                <hr />
             </div>
-            <p>{result.description}</p>
-        </div>
     )
 }

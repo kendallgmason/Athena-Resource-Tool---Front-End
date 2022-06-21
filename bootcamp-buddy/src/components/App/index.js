5721fb4e827d22156a5d2630fcfa9ec6dd62083f
danielhawkesford <daniel.hawkesford@gmail.com>
2022-06-21 10:37:08 +0100

    Built Favourites component

--- bootcamp-buddy/src/components/App/index.js
+++ bootcamp-buddy/src/components/App/index.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Favourite } from '../Favourite';
 
 function App() {
   return (
     <div className="App">
     Hello 
+      <Favourite favourite={{title: 'Learn git', URL: "https://learngitbranching.js.org/"}} />
     </div>
   );
 }

2463e069ef71ff9c3b5ff995289d9ccece9d96dc
Jenan <j.azzubaydi@gmail.com>
2022-06-22 11:29:06 +0100

    error

--- bootcamp-buddy/src/components/Dropdown/index.js
+++ bootcamp-buddy/src/components/Dropdown/index.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 export function Dropdown ({handleChange}) {
+
+  const[value,setValue] = useState('');
+
+  function handleSelect(e){
+    setValue(e.target.value);
+    console.log(value)
+  }
   return (
  
       <div>
           <label htmlFor="Categories"> Type Of Topic </label>
-          <select onChange={handleChange} name="categories" id="Categories">
+          <select onChange={handleChange} onSelect={handleSelect} name="categories" id="Categories">
           <option className="dropdown-option" ></option>
           <option className="dropdown-option" >React</option>
-          <option className="dropdown-option" >jest</option>
-          <option className="dropdown-option" >git</option>
+          <option className="dropdown-option" >Jest</option>
+          <option className="dropdown-option" >Git</option>
           </select>
 
       </div>
--- bootcamp-buddy/src/components/PostLink/index.js
+++ bootcamp-buddy/src/components/PostLink/index.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
+import { Typedropdown } from '../App/Typedropdown/index.js';
+import { Dropdown } from '../Dropdown/index.js';
 
-export function PostLink({ handleClick }){
+export function PostLink({ handleClick , onSelect}){
+
   const[resource, setResource] = useState({
+    topic: onSelect,
+    type:"",
     URL: "",
     title: "",
     description: ""
@@ -13,12 +18,25 @@ export function PostLink({ handleClick }){
       ...resource,
       [name]: value
     })
+    console.log(resource)
   }
-
+ //----------- 
+// const [val,setVal]= useState()
+// function handlechanging(e){
+//   setVal(e.target.value)
+//   console.log(setVal)
+// }
+//-----------
   return(
     <div className="post-link-content">
       <h2>Put your new links here</h2>
       <div className="fields">
+      <div className="wrapper-item1">
+        <Dropdown name="topic" value={resource.topic} onChange={handleInputChange}></Dropdown>
+        </div>
+        <div className="wrapper-item1">
+        <Typedropdown name="type" value={resource.type} onChange={handleInputChange}></Typedropdown>
+        </div>
         <div className="wrapper-item1">
           <input type="text" name="URL" value={resource.URL} placeholder='URL' onChange={handleInputChange}/>
         </div>

0300fb90751fcfdfc6aab826fea5088ef594d6b3
Jenan <j.azzubaydi@gmail.com>
2022-06-22 15:50:16 +0100

    search component testing not done yet

---
+++ bootcamp-buddy/src/components/Search/Search.test.js
@@ -0,0 +1,22 @@
+
+// import { screen} from "@testing-library/react";
+// // const testProps = {
+// //     list: ["item 1", "item 2"],
+// //     tickItem: jest.fn(),
+// //  };
+// // test("postLink renders successfully", () => {
+// //     const { getByTestId } = render(<Result {...testProps} />);
+// //     const actual = getByTestId("object");
+// //     expect(actual).toBeInTheDocument();
+// //  });
+
+// //---------------------------------
+
+// test("testing search button", ()=> {
+
+//     // const input = screen.queryByText('input')
+//     // expect(input).toBeInTheDocument()
+
+//     const searchButton = screen.queryByText('Search')
+//     expect(searchButton).toBeInTheDocument()
+// })

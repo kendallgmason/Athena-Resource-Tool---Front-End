a47aadb1dad28a75579d76137d8274ab7b3132fc
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-06-22 11:37:14 +0100

    Adjusted the dropdown box titles

--- bootcamp-buddy/src/components/App/Typedropdown/index.js
+++ bootcamp-buddy/src/components/App/Typedropdown/index.js
@@ -1,9 +1,9 @@
 export function Typedropdown ({handleChange}) {
     return (
         <div className="typeDropdown">
-          <label htmlFor="Categories"> Type </label>
+          <label htmlFor="Categories"> </label>
           <select onChange={handleChange} name="categories" id="another-categories">
-          <option className="dropdown-option" >All</option>
+          <option className="dropdown-option" >Type</option>
           <option className="dropdown-option" >Documentation</option>
           <option className="dropdown-option" >Article</option>
           <option className="dropdown-option" >Game/App</option>

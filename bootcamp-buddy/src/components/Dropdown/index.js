2aae699fafd8fcfa4084415d2039b510ba33e21b
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-06-22 11:35:19 +0100

    Changes

--- bootcamp-buddy/src/components/Dropdown/index.js
+++ bootcamp-buddy/src/components/Dropdown/index.js
@@ -2,9 +2,9 @@ export function Dropdown ({handleChange}) {
   return (
  
       <div className="dropdown">
-          <label htmlFor="Categories"> Topic </label>
+          <label htmlFor="Categories"></label>
           <select onChange={handleChange} name="categories" id="Categories">
-          <option className="dropdown-option" >All</option>
+          <option className="dropdown-option" >Topic</option>
           <option className="dropdown-option" >React</option>
           <option className="dropdown-option" >Jest</option>
           <option className="dropdown-option" >Git</option>
